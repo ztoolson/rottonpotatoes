0a54f7975b9ff5a44b8f1cf3fbc4cf1adf5dbccd
Zach Toolson <zach.toolson@gmail.com>
2015-07-12 16:01:46 -0600

    Create movie model. Set up movie seed data.
    The movie model uses ActiveRecord as an ORM to map models to database
    table names. Movie(model) -> movies(database table)
    
    Self Check
    1. Why are `where` and `find` class methods rather than instance method?
    
    Instance methods operate on one instance of the class, but until we look
    up one or more objects, we have no instances to operate on.
    
    2. Do Rails models acquire the methods `where` and `find` via (a)
    inheritance, or (b) mix-in?
    
    They inherit from the ActiveRecord::Base.
    
    NOTE:
    ActiveRecord uses convention over configuration to infer databse table
    names from the names on model classes, and to infer the names and types
    of the columns (attributes) associated with a given kind of model.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+movies = [
+  { title: 'Star Wars',               rating: 'PG',    release_date: '25/5/1977' },
+  { title: 'Field of Dreams',         rating: 'PG',    release_date: '21-Apr-89' },
+  { title: 'Aladdin',                 rating: 'G',     release_date: '25-Nov-1992' },
+  { title: 'When Harry Met Sally',    rating: 'R',     release_date: '21-Jul-1989' },
+  { title: 'The Help',                rating: 'PG-13', release_date: '10-Aug-2011' },
+  { title: 'Raiders of the Lost Ark', rating: 'PG',    release_date: '12-Dec-1981' },
+]
+
+movies.each do |movie|
+  Movie.create!(movie)
+end
