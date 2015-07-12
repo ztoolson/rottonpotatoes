6237777b40cc5a94ad0a8e3dbd60342f78163e25
Zach Toolson <zach.toolson@gmail.com>
2015-07-12 16:59:47 -0600

    Complete movies controllers and associated views.
    Self Check
    1. Why don't the controller methods for the `new` action (new_movie_path)
    and `create` action (movies_path) take any arguements when the `show`
    and `update` helpers do?
    
    Show and update operate on existing movies, so they take an argument to
    identify which movie to operation on. New and fcreate by definition
    operate on not-yet-existing movies.
    
    2. Why doesn't the helper method for the Index action take an argument?
    
    Index just shows a list of all the movies, so no argument is needed to
    distinguish which movie to operate on.
    
    3. Why is there no end corresponding to the `do` in haml templates?
    
    Unlike Ruby itself, Haml relies on indentation to indicate nesting, so
    the end is supplied by Haml when executing the Ruby code in the `do`.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,44 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def show
+    @movie = movie_from_params_id
+  end
+
+  def new
+  end
+
+  def create
+    @movie = Movie.create(movie_params)
+    flash[:notice] = "#{@movie.title} successfully created"
+    redirect_to_movies_path
+  end
+
+  def edit
+    @movie = movie_from_params_id
+  end
+
+  def update
+    @movie = movie_from_params_id.update_attributes(movie_params)
+    flash[:notice] = "#{@movie.title} successfully updated"
+    redirect_to_movies_path
+  end
+
+  def destroy
+    @movie = movie_from_params_id
+    @movie.destroy
+    flash[:notice] = "#{@movie.title} has been deleted"
+    redirect_to_movies_path
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
+
+  def movie_from_params_id
+    @movie = Movie.find_by_id(params[:id])
+  end
 end
